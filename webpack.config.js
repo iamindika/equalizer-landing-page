f99ec228ce7665afcfcd6812ad5175a0bb5c6626
iamindika <ikolamba2@gmail.com>
2022-01-31 20:14:58 -0500

    add webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  }, 
+  module: {
+    rules: [
+      { test: /\.(svg | png)/, type: 'asset/resource' },
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
