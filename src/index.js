8ead27c0413fd5e4d9c1d535ace2e336b9bc1380
iamindika <ikolamba2@gmail.com>
2022-02-04 03:37:24 -0500

    add body styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font: 400 1.125rem 'IBM Plex Sans', sans-serif;
+  }
+`;
 
 ReactDOM.render(
-  <App />,
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
   document.getElementById('app')
 );
