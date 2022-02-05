3a260437a8f8852c4b88169f9796d04ad2e4cb3a
iamindika <ikolamba2@gmail.com>
2022-02-05 11:53:13 -0500

    render global styles, intro and main wrapper

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import IntroWrapper from './components/IntroWrapper';
+import MainWrapper from './components/MainWrapper';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *,
+  *::before,
+  *::after {
     margin: 0;
-    font: 400 1.125rem 'IBM Plex Sans', sans-serif;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: hsl(20, 33%, 98%);  
+    font: 400 1.6rem / 1.625 'IBM Plex Sans', sans-serif;
   }
 `;
 
 ReactDOM.render(
   <React.Fragment>
     <GlobalStyle />
-    <App />
+    <IntroWrapper />
+    <MainWrapper />
   </React.Fragment>,
   document.getElementById('app')
 );
